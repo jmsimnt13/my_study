4a9e0199f46f09a07db029e82dfd8e32ece2e0fd
Danila <Doe>
2025-02-20 22:44:14 +0300

    + howe assigment

---
+++ My_Tasks/Conditions_lvl_1.swift
@@ -0,0 +1,12 @@
+//
+//  Conditions_lvl_1.swift
+//  My_Tasks
+//
+//  Created by Данила on 20.02.2025.
+//
+
+import Foundation
+
+print("Enter the number of task from 1 to 5")
+let choice =
+
---
+++ Study_Tasks/Study_Tasks/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Study_Tasks
+//
+//  Created by Данила on 20.02.2025.
+//
+
+import Foundation
+
+print("Hello, World!")
+
