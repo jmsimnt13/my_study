144b2e3fd513c33636ea5250edb5a7a4978e4048
Daniel Doe <>
2025-04-05 13:46:40 +0300

    Создание первого тестового проекта приложения для iOS

---
+++ iOSTestApp/iOSTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTestApp
+//
+//  Created by Данила on 05.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
