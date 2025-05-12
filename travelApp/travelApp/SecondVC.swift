b6b34d2dc6b3cbb1f69e9c5b0cef772faefbab24
Daniel Doe <>
2025-05-12 21:45:53 +0300

    Создание болванки приложения, подготовка для верстки кодом, удаление сториборда, настройка scenedelegate, добавление красного фона и главного VC

---
+++ travelApp/travelApp/SecondVC.swift
@@ -0,0 +1,8 @@
+//
+//  SecondVC.swift
+//  travelApp
+//
+//  Created by Данила on 12.05.2025.
+//
+
+import Foundation
---
+++ travelApp/travelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  travelApp
+//
+//  Created by Данила on 12.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		view.backgroundColor = .red
+	}
+
+
+}
+
